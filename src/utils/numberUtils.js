83ec7cf4b37af0e4787f45d2a027760464d2dc46
Mahdi <beykipour@gmail.com>
2019-08-12 03:41:25 +0430

    Add js doc for util functions

--- src/utils/numberUtils.js
+++ src/utils/numberUtils.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {number} num the number to be rounded
+ * @param {number} fractionLength the maximum length of the decimals
+ * @returns {number}
+ */
 export const roundNumber = (num, fractionLength = 12) => {
     return parseFloat(Number(num.toString()).toFixed(fractionLength));
 };
