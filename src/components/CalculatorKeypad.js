1caad5b0ff6e4e8e1e49b6a5e988c7a6bff8358c
Mahdi <beykipour@gmail.com>
2019-08-11 18:42:21 +0430

    Add keypad layout and styles

--- src/components/CalculatorKeypad.js
+++ src/components/CalculatorKeypad.js
@@ -6,6 +6,25 @@ class CalculatorKeypad extends React.PureComponent {
         return (
             <div className={'c-calculator__keypad'}>
 
+                <button className={"c-calculator__key c-calculator__key--num"} value="7">7</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="8">8</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="9">9</button>
+                <button className={"c-calculator__key c-calculator__key--operator"} value="+">+</button>
+
+                <button className={"c-calculator__key c-calculator__key--num"} value="4">4</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="5">5</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="6">6</button>
+                <button className={"c-calculator__key c-calculator__key--operator"} value="-">-</button>
+
+                <button className={"c-calculator__key c-calculator__key--num"} value="1">1</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="2">2</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="3">3</button>
+                <button className={"c-calculator__key c-calculator__key--equal"} value="=">=</button>
+
+                <button className={"c-calculator__key c-calculator__key--ac"} value="ac">AC</button>
+                <button className={"c-calculator__key c-calculator__key--num"} value="0">0</button>
+                <button className={"c-calculator__key c-calculator__key--decimal"} value=".">.</button>
+
             </div>
         );
     }
